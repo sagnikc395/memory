c7a538666f88347ffe17290dd2e50afc3250124b
Sagnik Chatterjee <sagnikchatterjee607@gmail.com>
2026-02-09 18:22:39 -0500

    feat: added support for hf token

--- memory/generate_embeddings.py
+++ memory/generate_embeddings.py
@@ -1,5 +1,9 @@
 from sentence_transformers import SentenceTransformer
 from .config import EMBEDDING_MODEL
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 # groq doesnt have embeddings models, using sentence-transformers
@@ -16,7 +20,7 @@ async def generate_embeddings(strings: list[str]):
     if not EMBEDDING_MODEL:
         raise ValueError("EMBEDDING_MODEL not set in config.py")
 
-    model = SentenceTransformer(EMBEDDING_MODEL)
+    model = SentenceTransformer(EMBEDDING_MODEL, token=os.environ.get("HF_TOKEN"))
     embeddings = model.encode(strings)
     # embeddings = np.stack([item.embedding for item in out.data])
     print(f"Embeddings shape: {embeddings.shape}")
